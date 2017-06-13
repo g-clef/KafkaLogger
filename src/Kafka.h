4ede474ef8da19968460698e58f89de0c29fff0d
Jérémie Banier <jeremie.banier@proximus.com>
2017-06-13 15:13:29 +0200

    Adding ssl and kerberos support to the kafka plugin

--- src/Kafka.h
+++ src/Kafka.h
@@ -54,6 +54,14 @@ private:
 	int client_id_len;
 	char* compression_codec;
 	int compression_codec_len;
+	char* security_protocol;
+	int security_protocol_len;
+	char* service_name;
+	int service_name_len;
+	char* kerberos_keytab;
+	int kerberos_keytab_len;
+	char* kerberos_principal;
+	int kerberos_principal_len;
 	RdKafka::Conf *conf;
 	RdKafka::Conf *tconf;
 	RdKafka::Producer *producer;
